f62649bffef8fbc23074d6d28ca8ed3595ddee69
sigavg <lesimplu@gmail.com>
2022-04-30 20:32:49 -0500

    1

---
+++ Assignment 9 NavMesh/Assets/Scripts/MoveTo.cs
@@ -0,0 +1,27 @@
+﻿/* Luke Lesimple
+ * Assigment 9
+ * Moves player to target
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveTo : MonoBehaviour
+{
+    public Transform goal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
